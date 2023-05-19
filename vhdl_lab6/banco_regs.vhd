b9f219de5dcb1b7f2570063da7fd65a32f56bac3
Lee3007 <felipe.lee30@gmail.com>
2023-05-18 22:04:16 -0300

    Started LAB6

---
+++ vhdl_lab6/banco_regs.vhd
@@ -0,0 +1,78 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity banco_regs is
+    port(
+        --selecionando regs
+        out_sel: in unsigned(2 downto 0);
+        write_sel: in unsigned(2 downto 0);
+        
+        --fluxo de dados
+        data_in: in unsigned(15 downto 0);
+        out_reg: out unsigned(15 downto 0);
+        
+        --controle
+        clk: in std_logic;
+        rst: in std_logic;
+        write_enable: in std_logic
+    );
+end;
+
+architecture arch_banco_regs of banco_regs is
+
+    component reg_16bits is
+        port(
+            reg_in: in unsigned(15 downto 0);
+            rst: in std_logic;
+            clk: in std_logic;
+            write_enable: in std_logic;
+            reg_out: out unsigned (15 downto 0)
+        );
+    end component;
+
+    type output_array is array (7 downto 0) of unsigned(15 downto 0);
+
+    signal out_signal: unsigned(15 downto 0);
+    signal write_enables: std_logic_vector(7 downto 0);
+    signal out_buses: output_array;
+
+    begin
+        reg_0: reg_16bits port map(
+            reg_in => X"0000",
+            rst => rst,
+            clk => clk,
+            write_enable => write_enables(0),
+            reg_out => out_buses(0)
+        );
+
+        gen_regs: for i in 1 to 7 generate
+            reg_i: reg_16bits port map(
+                reg_in => data_in,
+                rst => rst,
+                clk => clk,
+                write_enable => write_enables(i),
+                reg_out => out_buses(i)
+            ); --do reg => do banco
+        end generate gen_regs;
+
+        process(clk, rst)
+        begin
+
+            if(rst = '1') then
+                out_signal <= X"0000";
+            else
+                if(rising_edge(clk)) then
+                    out_signal <= out_buses(to_integer(out_sel));
+                end if;
+            end if;
+
+        end process;
+
+        gen_write_enables: for i in 0 to 7 generate
+            write_enables(i) <= write_enable when to_integer(write_sel) = i else '0';
+        end generate gen_write_enables;
+
+        out_reg <= out_signal;
+
+end architecture;
---
+++ vhdl_lab6/toplevel_tb.vhd
@@ -0,0 +1,74 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity toplevel_tb is
+end entity;
+
+architecture toplevel_tb_a of toplevel_tb is
+    
+    component toplevel is
+        port (
+            clock: in std_logic;
+            rst: in std_logic;
+            state: out unsigned(1 downto 0);
+            pc_out: out unsigned(7 downto 0);
+            instruction: out unsigned(15 downto 0);
+            reg_a: out unsigned(15 downto 0);
+            reg_b: out unsigned(15 downto 0);
+            result: out unsigned(15 downto 0);
+            zero, carry: out std_logic
+        );
+    end component;
+
+    signal clk_s, rst_s, zero_s, carry_s: std_logic;
+    signal state_s: unsigned(1 downto 0);
+    signal pc_s: unsigned(7 downto 0);
+    signal instruction_s, reg_a_s, reg_b_s, result_s: unsigned(15 downto 0);
+
+    constant period_time : time := 100 ns;
+    signal finished : std_logic := '0';
+
+    begin
+
+        topleveluut: toplevel port map(
+            clock => clk_s,
+            rst => rst_s,
+            state => state_s,
+            pc_out => pc_s,
+            instruction => instruction_s,
+            reg_a => reg_a_s,
+            reg_b => reg_b_s,
+            result => result_s,
+            zero => zero_s,
+            carry => carry_s
+        );
+
+        sim_time_proc: process
+        begin
+            wait for 10 us;
+            finished <= '1';
+            wait;
+        end process sim_time_proc;
+
+        clk_proc: process
+        begin
+            while finished /= '1' loop
+            clk_s <= '0';
+            wait for period_time/2;
+            clk_s <= '1';
+            wait for period_time/2;
+            end loop;
+            wait;
+        end process clk_proc;
+
+        process
+        begin
+            rst_s <= '1';
+            wait for 200 ns;
+            rst_s <= '0';
+
+            wait;
+        end process;
+
+end architecture;
